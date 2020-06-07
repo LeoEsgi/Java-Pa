6e17d85a33f8ec8fbd8dcdded6131c0c4bb0911f
Léo <Daviere-leo@hotmail.fr>
2020-06-07 14:38:04 +0200

    Update Tests

--- pa/PaTest.java
+++ pa/PaTest.java
@@ -5,6 +5,10 @@ import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
 
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
 import static org.junit.Assert.*;
 
 public class PaTest {
@@ -15,12 +19,50 @@ public class PaTest {
 
         Account A = new Account("jean", "petigros","Marseille", "France", Account.AccountType.CUSTOMER, true);
 
-        assertEquals(A.getFirstname(), "jean");
-        assertEquals(A.getLastname(), "petigros");
-        assertEquals(A.getCity(), "Marseille");
-        assertEquals(A.getCountry(), "France");
-        assertEquals(A.getType(), Account.AccountType.CUSTOMER);
-        assertEquals(A.isActivated(), true);
+        assertEquals(A.toString(), "Account{" +
+                "firstname='" + "jean" + '\'' +
+                ", lastname='" + "petigros" + '\'' +
+                ", city='" + "Marseille" + '\'' +
+                ", country='" + "France" + '\'' +
+                ", type=" + Account.AccountType.CUSTOMER +
+                ", activated=" + true +
+                '}');
+
+    }
+
+    @Test
+    public void testCreateProduct() {
+
+        Product P = new Product("pomme", Product.ProductType.DESSERT, 1.50 , 20);
+
+        assertEquals(P.toString(), "Product{" +
+                "name='" + "pomme" + '\'' +
+                ", type=" + Product.ProductType.DESSERT +
+                ", price=" + 1.50 +
+                ", size=" + 20 +
+                '}');
+
+
+    }
+
+    @Test
+    public void testCreateMenu() {
+
+
+        Product P = new Product("pomme", Product.ProductType.DESSERT, 1.50 , 20);
+        List<Product> productList = new ArrayList<>();
+        productList.add(P);
+        Date date = new Date();
+
+        Menu M = new Menu("MenuTest" , productList, date, 10.50);
+
+        assertEquals(M.toString(), "Menu{" +
+                "name='" + "MenuTest" + '\'' +
+                ", content=" + productList +
+                ", creationDate=" + date +
+                ", price=" + 10.50 +
+                '}');
+
 
     }
 
